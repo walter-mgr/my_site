7009bcdf1678d52db55c68faf1c32a9b31027c46
Valeriy Borzak <valeraborzak@gmail.com>
2024-07-07 15:22:17 +0200

    Fix status - CharField

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.Choices(max_length=2, choices=Status, default=Status.DRAFT)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
 
     class Meta:
         ordering = ["-publish"]
