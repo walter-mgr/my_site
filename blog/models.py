16ed08e26c5b770d542e53f694640f3008b19368
Valeriy Borzak <valeraborzak@gmail.com>
2024-07-27 21:00:57 +0200

    Modifying blog/urls.py, views.py, models.py

--- blog/models.py
+++ blog/models.py
@@ -38,4 +38,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.pk])  # control pk
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )  # control pk
