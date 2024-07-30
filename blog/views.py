ee7a28ba78b568d73431b39f0532c5225f9e038d
Valeriy Borzak <valeraborzak@gmail.com>
2024-07-30 20:14:51 +0200

    Build a class-based Post List View

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 # from django.http import Http404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 from .models import Post
 
-
+"""
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
@@ -16,6 +17,14 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
 
     return render(request, "blog/post/list.html", {"posts": posts})
+"""
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 """
